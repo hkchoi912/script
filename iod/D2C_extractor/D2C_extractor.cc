38c27d0e1878ffcb89fa270b41eda979e8d3464d
hkchoi <hkchoi@camelab.org>
2021-10-05 16:26:52 +0900

    fix segmentation error

--- iod/D2C_extractor/D2C_extractor.cc
+++ iod/D2C_extractor/D2C_extractor.cc
@@ -1,32 +1,29 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <regex>
-#include <cinttypes>
-#include <queue>
-
 #include <errno.h>
-#include <unistd.h>
 #include <fcntl.h>
 #include <libaio.h>
-#include <time.h>
-#include <sys/ioctl.h>
-#include <sys/stat.h>
 #include <linux/fs.h>
 #include <stddef.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <cinttypes>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <regex>
 #include <string>
 
-#define MAX_DEPTH 10000
+#define MAX_DEPTH 100
 
-std::string double_to_string(double t)
-{
+std::string double_to_string(double t) {
     std::stringstream sstr;
     sstr << t;
     return sstr.str();
 }
 
-struct regexio
-{
+struct regexio {
     __time_t tv_sec;
     __syscall_slong_t tv_nsec;
     const char *action;
@@ -40,8 +37,7 @@ struct regexio
 // std::regex regex_blkparse("\\d+,\\d+ +\\d+ +\\d+ +(\\d+)\\.(\\d+) +\\d+ +(\\w) +(\\w+) +(\\d+) \\+ (\\d+).*");
 std::regex regex_blkparse("\\d+,\\d+ +\\d+ +\\d+ +(\\d+)\\.(\\d+) +\\d+ +(\\w) +(\\w+) +(\\d+) \\+ (\\d+) +\\[(.*)\\].*");
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     char *tracepath = nullptr;
     char *readpath = nullptr;
     char *writepath = nullptr;
@@ -61,14 +57,12 @@ int main(int argc, char *argv[])
     std::string strout;
 
     // Read device name, trace file and delay
-    if (argc == 4)
-    {
+    if (argc == 4) {
         tracepath = argv[1];
         readpath = argv[2];
         writepath = argv[3];
     }
-    if (argc != 4)
-    {
+    if (argc != 4) {
         std::cerr << "Invalid number of arguments." << std::endl;
         std::cerr << argv[0] << " <trace file> <read output file> <write output file>" << std::endl;
         return 1;
@@ -76,19 +70,17 @@ int main(int argc, char *argv[])
 
     // Open trace file
     trace.open(tracepath);
-    readoutput.open(readpath, std::ofstream::out | std::ofstream::trunc);   // open with delete contents
-    writeoutput.open(writepath, std::ofstream::out | std::ofstream::trunc); // open with delete contents
+    readoutput.open(readpath, std::ofstream::out | std::ofstream::trunc);    // open with delete contents
+    writeoutput.open(writepath, std::ofstream::out | std::ofstream::trunc);  // open with delete contents
 
-    if (!trace.is_open())
-    {
+    if (!trace.is_open()) {
         std::cerr << "Failed to open trace file." << std::endl;
         return 1;
     }
 
     std::cout << "Begin parsing." << std::endl;
 
-    while (!trace.eof())
-    {
+    while (!trace.eof()) {
         int ret;
         std::string line;
         std::smatch match;
@@ -105,19 +97,15 @@ int main(int argc, char *argv[])
          * match[6] : length
          * match[7] : workload
          */
-        if (std::regex_match(line, match, regex_blkparse))
-        {
-            if (match[3].str().at(0) == 'D')
-            {
+        if (std::regex_match(line, match, regex_blkparse)) {
+            if (match[3].str().at(0) == 'D') {
                 char rwsb = match[4].str().at(0);
 
-                if (rwsb != 'R' && rwsb != 'W')
-                {
+                if (rwsb != 'R' && rwsb != 'W') {
                     continue;
                 }
 
-                if (rwsb == 'R')
-                {
+                if (rwsb == 'R') {
                     readDlog[i].tv_sec = (__time_t)strtoull(match[1].str().c_str(), nullptr, 10);
                     readDlog[i].tv_nsec = (__syscall_slong_t)strtoull(match[2].str().c_str(), nullptr, 10);
                     readDlog[i].action = match[3].str().c_str();
@@ -130,9 +118,7 @@ int main(int argc, char *argv[])
                         i++;
                     else
                         i = 0;
-                }
-                else
-                {
+                } else {
                     writeDlog[j].tv_sec = (__time_t)strtoull(match[1].str().c_str(), nullptr, 10);
                     writeDlog[j].tv_nsec = (__syscall_slong_t)strtoull(match[2].str().c_str(), nullptr, 10);
                     writeDlog[j].action = match[3].str().c_str();
@@ -146,46 +132,39 @@ int main(int argc, char *argv[])
                     else
                         j = 0;
                 }
-            }
-            else if (match[3].str().at(0) == 'C')
-            {
+            } else if (match[3].str().at(0) == 'C') {
                 char rwsb = match[4].str().at(0);
                 readfind = false;
                 writefind = false;
 
-                if (rwsb != 'R' && rwsb != 'W')
-                {
+                if (rwsb != 'R' && rwsb != 'W') {
                     continue;
                 }
 
-                if (rwsb == 'R')
-                {
-                    if (i != 0)
-                    {
-                        for (k = i - 1; k >= 0; k--)
-                        {
-                            if (readDlog[k].offset == (uint64_t)strtoull(match[5].str().c_str(), nullptr, 10))
-                            {
+                if (rwsb == 'R') {
+                    if (i != 0) {
+                        for (k = i - 1; k >= 0; k--) {
+                            if (readDlog[k].offset == (uint64_t)strtoull(match[5].str().c_str(), nullptr, 10)) {
                                 Ctime.tv_sec = (__time_t)strtoull(match[1].str().c_str(), nullptr, 10);
                                 Ctime.tv_nsec = (__syscall_slong_t)strtoull(match[2].str().c_str(), nullptr, 10);
                                 Dtime.tv_sec = readDlog[k].tv_sec;
                                 Dtime.tv_nsec = readDlog[k].tv_nsec;
 
-                                if (Ctime.tv_nsec - Dtime.tv_nsec >= 0)
-                                {
+                                if (Ctime.tv_nsec - Dtime.tv_nsec >= 0) {
                                     D2Ctime.tv_sec = Ctime.tv_sec - Dtime.tv_sec;
                                     D2Ctime.tv_nsec = Ctime.tv_nsec - Dtime.tv_nsec;
-                                }
-                                else
-                                {
+                                } else {
                                     D2Ctime.tv_sec = Ctime.tv_sec - Dtime.tv_sec - 1;
                                     D2Ctime.tv_nsec = 1000000000 + Ctime.tv_nsec - Dtime.tv_nsec;
                                 }
 
                                 strout = "";
                                 strout += double_to_string(Dtime.tv_sec + (double)Dtime.tv_nsec / (double)1000000000) + " ";
-                                // strout += double_to_string(D2Ctime.tv_sec * 1000000 + (double)D2Ctime.tv_nsec / (double)1000) + " " +readDlog[k].workload + "\n";
-                                strout += double_to_string(D2Ctime.tv_sec * 1000000 + (double)D2Ctime.tv_nsec / (double)1000) + "\n";
+                                strout += double_to_string(D2Ctime.tv_sec * 1000000 + (double)D2Ctime.tv_nsec / (double)1000) + " ";
+                                strout += double_to_string(readDlog[k].length) + " ";
+                                strout += readDlog[k].workload;
+                                strout += "\n";
+
                                 readoutput.write(strout.c_str(), strout.length());
 
                                 readfind = true;
@@ -193,32 +172,29 @@ int main(int argc, char *argv[])
                             }
                         }
                     }
-                    if (!readfind)
-                    {
-                        for (k = MAX_DEPTH - 1; k >= i; k--)
-                        {
-                            if (readDlog[k].offset == (uint64_t)strtoull(match[5].str().c_str(), nullptr, 10))
-                            {
+                    if (!readfind) {
+                        for (k = MAX_DEPTH - 1; k >= i + 1; k--) {
+                            if (readDlog[k].offset == (uint64_t)strtoull(match[5].str().c_str(), nullptr, 10)) {
                                 Ctime.tv_sec = (__time_t)strtoull(match[1].str().c_str(), nullptr, 10);
                                 Ctime.tv_nsec = (__syscall_slong_t)strtoull(match[2].str().c_str(), nullptr, 10);
                                 Dtime.tv_sec = readDlog[k].tv_sec;
                                 Dtime.tv_nsec = readDlog[k].tv_nsec;
 
-                                if (Ctime.tv_nsec - Dtime.tv_nsec >= 0)
-                                {
+                                if (Ctime.tv_nsec - Dtime.tv_nsec >= 0) {
                                     D2Ctime.tv_sec = Ctime.tv_sec - Dtime.tv_sec;
                                     D2Ctime.tv_nsec = Ctime.tv_nsec - Dtime.tv_nsec;
-                                }
-                                else
-                                {
+                                } else {
                                     D2Ctime.tv_sec = Ctime.tv_sec - Dtime.tv_sec - 1;
                                     D2Ctime.tv_nsec = 1000000000 + Ctime.tv_nsec - Dtime.tv_nsec;
                                 }
 
                                 strout = "";
                                 strout += double_to_string(Dtime.tv_sec + (double)Dtime.tv_nsec / (double)1000000000) + " ";
-                                // strout += double_to_string(D2Ctime.tv_sec * 1000000 + (double)D2Ctime.tv_nsec / (double)1000) + " " +readDlog[k].workload + "\n";
-                                strout += double_to_string(D2Ctime.tv_sec * 1000000 + (double)D2Ctime.tv_nsec / (double)1000) + "\n";
+                                strout += double_to_string(D2Ctime.tv_sec * 1000000 + (double)D2Ctime.tv_nsec / (double)1000) + " ";
+                                strout += double_to_string(readDlog[k].length) + " ";
+                                strout += readDlog[k].workload;
+                                strout += "\n";
+
                                 readoutput.write(strout.c_str(), strout.length());
 
                                 readfind = true;
@@ -226,35 +202,31 @@ int main(int argc, char *argv[])
                             }
                         }
                     }
-                }
-                else
-                {
-                    if (j != 0)
-                    {
-                        for (l = j - 1; l >= 0; l--)
-                        {
-                            if (writeDlog[l].offset == (uint64_t)strtoull(match[5].str().c_str(), nullptr, 10))
-                            {
+                } else {
+                    if (j != 0) {
+                        // find same offset in writelog[0]~[j-1]
+                        for (l = j - 1; l >= 0; l--) {
+                            if (writeDlog[l].offset == (uint64_t)strtoull(match[5].str().c_str(), nullptr, 10)) {
                                 Ctime.tv_sec = (__time_t)strtoull(match[1].str().c_str(), nullptr, 10);
                                 Ctime.tv_nsec = (__syscall_slong_t)strtoull(match[2].str().c_str(), nullptr, 10);
                                 Dtime.tv_sec = writeDlog[l].tv_sec;
                                 Dtime.tv_nsec = writeDlog[l].tv_nsec;
 
-                                if (Ctime.tv_nsec - Dtime.tv_nsec >= 0)
-                                {
+                                if (Ctime.tv_nsec - Dtime.tv_nsec >= 0) {
                                     D2Ctime.tv_sec = Ctime.tv_sec - Dtime.tv_sec;
                                     D2Ctime.tv_nsec = Ctime.tv_nsec - Dtime.tv_nsec;
-                                }
-                                else
-                                {
+                                } else {
                                     D2Ctime.tv_sec = Ctime.tv_sec - Dtime.tv_sec - 1;
                                     D2Ctime.tv_nsec = 1000000000 + Ctime.tv_nsec - Dtime.tv_nsec;
                                 }
 
                                 strout = "";
                                 strout += double_to_string(Dtime.tv_sec + (double)Dtime.tv_nsec / (double)1000000000) + " ";
-                                // strout += double_to_string(D2Ctime.tv_sec * 1000000 + (double)D2Ctime.tv_nsec / (double)1000) + " " +writeDlog[l].workload + "\n";
-                                strout += double_to_string(D2Ctime.tv_sec * 1000000 + (double)D2Ctime.tv_nsec / (double)1000) + "\n";
+                                strout += double_to_string(D2Ctime.tv_sec * 1000000 + (double)D2Ctime.tv_nsec / (double)1000) + " ";
+                                strout += double_to_string(writeDlog[l].length) + " ";
+                                strout += writeDlog[l].workload;
+                                strout += "\n";
+
                                 writeoutput.write(strout.c_str(), strout.length());
 
                                 writefind = true;
@@ -262,32 +234,30 @@ int main(int argc, char *argv[])
                             }
                         }
                     }
-                    if (!writefind)
-                    {
-                        for (l = MAX_DEPTH - 1; l >= j; l--)
-                        {
-                            if (writeDlog[l].offset == (uint64_t)strtoull(match[5].str().c_str(), nullptr, 10))
-                            {
+                    if (!writefind) {
+                        // if can't find in writelog[0]~[j-1], find in [j+1]~[MAX_DEPTH]
+                        for (l = MAX_DEPTH - 1; l >= j + 1; l--) {
+                            if (writeDlog[l].offset == (uint64_t)strtoull(match[5].str().c_str(), nullptr, 10)) {
                                 Ctime.tv_sec = (__time_t)strtoull(match[1].str().c_str(), nullptr, 10);
                                 Ctime.tv_nsec = (__syscall_slong_t)strtoull(match[2].str().c_str(), nullptr, 10);
                                 Dtime.tv_sec = writeDlog[l].tv_sec;
                                 Dtime.tv_nsec = writeDlog[l].tv_nsec;
 
-                                if (Ctime.tv_nsec - Dtime.tv_nsec >= 0)
-                                {
+                                if (Ctime.tv_nsec - Dtime.tv_nsec >= 0) {
                                     D2Ctime.tv_sec = Ctime.tv_sec - Dtime.tv_sec;
                                     D2Ctime.tv_nsec = Ctime.tv_nsec - Dtime.tv_nsec;
-                                }
-                                else
-                                {
+                                } else {
                                     D2Ctime.tv_sec = Ctime.tv_sec - Dtime.tv_sec - 1;
                                     D2Ctime.tv_nsec = 1000000000 + Ctime.tv_nsec - Dtime.tv_nsec;
                                 }
 
                                 strout = "";
                                 strout += double_to_string(Dtime.tv_sec + (double)Dtime.tv_nsec / (double)1000000000) + " ";
-                                // strout += double_to_string(D2Ctime.tv_sec * 1000000 + (double)D2Ctime.tv_nsec / (double)1000) + " " +readDlog[l].workload + "\n";
-                                strout += double_to_string(D2Ctime.tv_sec * 1000000 + (double)D2Ctime.tv_nsec / (double)1000) + "\n";
+                                strout += double_to_string(D2Ctime.tv_sec * 1000000 + (double)D2Ctime.tv_nsec / (double)1000) + " ";
+                                strout += double_to_string(writeDlog[l].length) + " ";
+                                strout += writeDlog[l].workload;
+                                strout += "\n";
+
                                 writeoutput.write(strout.c_str(), strout.length());
 
                                 writefind = true;
